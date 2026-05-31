e6dffd5feb55ab92f2a0c018d54417482303ff56
Rafie-Ghallap <rafeia.mustafa12@gmail.com>
2026-05-31 14:02:51 +0300

    delete enrollCourse.js

--- router/enrollmentRouter.js
+++ router/enrollmentRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const {enrollCourse} = require("../controllers/enrollment/enrollCourse");
+
 const { getMyEnrollments } = require("../controllers/enrollment/getMyEnrollments");
 const { unenrollCourse } = require("../controllers/enrollment/unenrollCourse");
 const {checkAuth} = require("../middleware/checkAuth");
@@ -10,7 +10,6 @@ const {checkRole} = require("../middleware/checkRole");
 router.use(checkAuth);
 router.use(checkRole(["student"]));
 
-router.post("/enroll/:id", enrollCourse);
 router.get("/myEnrollments", getMyEnrollments);
 router.delete("/unenroll/:id", unenrollCourse);
 
